d4123afbdf10a3b568254e8512816ec52e762a8e
Jeff Wendling <leterip@gmail.com>
2022-12-21 14:25:04 -0500

    update benchmark

--- bench_test.go
+++ bench_test.go
@@ -1,9 +1,7 @@
 package noiseconn
 
 import (
-	"bytes"
 	"crypto/rand"
-	"fmt"
 	"io"
 	"net"
 	"testing"
@@ -42,59 +40,70 @@ func osNetPipe() (client net.Conn, server net.Conn, err error) {
 }
 
 func BenchmarkThroughput(b *testing.B) {
-	defer try.F(b.Fatal)
-	p1, p2 := try.E2(osNetPipe())
+	run := func(size int64) func(*testing.B) {
+		return func(b *testing.B) {
+			defer try.F(b.Fatal)
+			p1, p2 := try.E2(osNetPipe())
 
-	clientKey := try.E1(noise.DH25519.GenerateKeypair(rand.Reader))
-	serverKey := try.E1(noise.DH25519.GenerateKeypair(rand.Reader))
+			clientKey := try.E1(noise.DH25519.GenerateKeypair(rand.Reader))
+			serverKey := try.E1(noise.DH25519.GenerateKeypair(rand.Reader))
 
-	client := try.E1(NewConn(p1, noise.Config{
-		CipherSuite:   noise.NewCipherSuite(noise.DH25519, noise.CipherChaChaPoly, noise.HashBLAKE2b),
-		Pattern:       noise.HandshakeIK,
-		Initiator:     true,
-		StaticKeypair: clientKey,
-		PeerStatic:    serverKey.Public,
-	}))
-	defer client.Close()
+			client := try.E1(NewConn(p1, noise.Config{
+				CipherSuite:   noise.NewCipherSuite(noise.DH25519, noise.CipherChaChaPoly, noise.HashBLAKE2b),
+				Pattern:       noise.HandshakeIK,
+				Initiator:     true,
+				StaticKeypair: clientKey,
+				PeerStatic:    serverKey.Public,
+			}))
+			defer client.Close()
 
-	server := try.E1(NewConn(p2, noise.Config{
-		CipherSuite:   noise.NewCipherSuite(noise.DH25519, noise.CipherChaChaPoly, noise.HashBLAKE2b),
-		Pattern:       noise.HandshakeIK,
-		Initiator:     false,
-		StaticKeypair: serverKey,
-	}))
-	defer server.Close()
+			server := try.E1(NewConn(p2, noise.Config{
+				CipherSuite:   noise.NewCipherSuite(noise.DH25519, noise.CipherChaChaPoly, noise.HashBLAKE2b),
+				Pattern:       noise.HandshakeIK,
+				Initiator:     false,
+				StaticKeypair: serverKey,
+			}))
+			defer server.Close()
 
-	data := make([]byte, 1024*b.N)
-	try.E1(rand.Read(data))
-	b.SetBytes(1024)
-	b.ResetTimer()
+			write := make([]byte, size)
+			read := make([]byte, size)
+			try.E1(rand.Read(write))
+			b.SetBytes(size)
+			b.ReportAllocs()
+			b.ResetTimer()
 
-	errch := make(chan error, 2)
-	go func() {
-		errch <- (func() (err error) {
-			defer try.Handle(&err)
-			try.E1(client.Write([]byte("get data")))
-			if !bytes.Equal(try.E1(io.ReadAll(io.LimitReader(client, int64(len(data))))), data) {
-				return fmt.Errorf("data mismatch")
-			}
-			return nil
-		})()
-	}()
-	go func() {
-		errch <- (func() (err error) {
-			defer try.Handle(&err)
-			cmd := make([]byte, len("get data"))
-			try.E1(io.ReadFull(server, cmd))
-			if !bytes.Equal(cmd, []byte("get data")) {
-				return fmt.Errorf("cmd mismatch")
-			}
-			try.E1(io.Copy(server, bytes.NewBuffer(data)))
-			return nil
-		})()
-	}()
-	try.E(<-errch)
-	try.E(<-errch)
-	try.E(client.Close())
-	try.E(server.Close())
+			errch := make(chan error, 2)
+			go func() {
+				errch <- (func() (err error) {
+					defer try.Handle(&err)
+					try.E1(client.Write([]byte("get data")))
+					for i := 0; i < b.N; i++ {
+						try.E1(io.ReadFull(client, read))
+					}
+					return nil
+				})()
+			}()
+			go func() {
+				errch <- (func() (err error) {
+					defer try.Handle(&err)
+					try.E1(io.ReadFull(server, make([]byte, len("get data"))))
+					for i := 0; i < b.N; i++ {
+						try.E1(server.Write(write))
+					}
+					return nil
+				})()
+			}()
+			try.E(<-errch)
+			try.E(<-errch)
+			try.E(client.Close())
+			try.E(server.Close())
+		}
+	}
+
+	b.Run("1K", run(1000))
+	b.Run("10K", run(1000*10))
+	b.Run("100K", run(1000*100))
+	b.Run("1M", run(1000*1000))
+	b.Run("10M", run(1000*10000))
+	b.Run("100M", run(1000*100000))
 }
