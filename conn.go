f7d18b8e31566bdec04b3211a9aa2794e378a752
JT Olio <hello@jtolio.com>
2022-12-21 13:02:54 -0500

    make it slower lol
    okay, but all that said, this reduces calls to the underlying
    the conn.Write a bunch

--- conn.go
+++ conn.go
@@ -16,11 +16,15 @@ const HeaderByte = 0x80
 // 65535 bytes! to my knowledge i don't know if this is actually a noise
 // requirement, but is at least a github.com/flynn/noise requirement.
 
+// TODO(jt): read and write cannot be called concurrently during handshake time
+
 type Conn struct {
 	net.Conn
 	initiator        bool
 	hs               *noise.HandshakeState
 	hsResponsibility bool
+	readMsgBuf       []byte
+	writeMsgBuf      []byte
 	readBuf          []byte
 	send, recv       *noise.CipherState
 }
@@ -50,13 +54,13 @@ func (c *Conn) setCipherStates(cs1, cs2 *noise.CipherState) {
 	}
 }
 
-func (c *Conn) hsRead() error {
-	m, err := c.readMsg(nil) // TODO(jt): nil
+func (c *Conn) hsRead() (err error) {
+	c.readMsgBuf, err = c.readMsg(c.readMsgBuf[:0])
 	if err != nil {
 		return err
 	}
 	var cs1, cs2 *noise.CipherState
-	c.readBuf, cs1, cs2, err = c.hs.ReadMessage(c.readBuf, m)
+	c.readBuf, cs1, cs2, err = c.hs.ReadMessage(c.readBuf, c.readMsgBuf)
 	if err != nil {
 		return errs.Wrap(err)
 	}
@@ -85,9 +89,13 @@ func (c *Conn) Read(b []byte) (n int, err error) {
 
 	for c.hs != nil {
 		if c.hsResponsibility {
-			err = c.hsWrite(nil)
+			c.writeMsgBuf, err = c.hsCreate(c.writeMsgBuf[:0], nil)
 			if err != nil {
 				return 0, err
+			}
+			_, err = c.Conn.Write(c.writeMsgBuf)
+			if err != nil {
+				return 0, errs.Wrap(err)
 			}
 			if c.hs == nil {
 				break
@@ -103,7 +111,7 @@ func (c *Conn) Read(b []byte) (n int, err error) {
 	}
 
 	for {
-		m, err := c.readMsg(nil) // TODO(jt): nil
+		c.readMsgBuf, err = c.readMsg(c.readMsgBuf[:0])
 		if err != nil {
 			return 0, err
 		}
@@ -114,7 +122,7 @@ func (c *Conn) Read(b []byte) (n int, err error) {
 		// from within m. it is also likely that
 		// the payload size is never larger than the
 		// message size and we could use that.
-		c.readBuf, err = c.recv.Decrypt(c.readBuf, nil, m)
+		c.readBuf, err = c.recv.Decrypt(c.readBuf, nil, c.readMsgBuf)
 		if err != nil {
 			return 0, errs.Wrap(err)
 		}
@@ -150,56 +158,40 @@ func (c *Conn) readMsg(b []byte) ([]byte, error) {
 	return b, nil
 }
 
-func (c *Conn) writeMsg(b []byte) error {
-	// TODO(jt): make sure these writes are through bufio somewhere in the stack
-	// appropriate.
-	var msgHeader [4]byte
+func (c *Conn) frame(header, b []byte) error {
 	if len(b) >= 1<<(8*3) {
 		return errs.New("message too large: %d", len(b))
 	}
-	binary.BigEndian.PutUint32(msgHeader[:], uint32(len(b)))
-	msgHeader[0] = HeaderByte
-	_, err := c.Conn.Write(msgHeader[:])
-	if err != nil {
-		// TODO(jt): close?
-		return errs.Wrap(err)
-	}
-	_, err = c.Conn.Write(b)
-	return errs.Wrap(err)
+	binary.BigEndian.PutUint32(header[:4], uint32(len(b)))
+	header[0] = HeaderByte
+	return nil
 }
 
-func (c *Conn) hsWrite(payload []byte) (err error) {
-	var out []byte // TODO(jt)
+func (c *Conn) hsCreate(out, payload []byte) (_ []byte, err error) {
 	var cs1, cs2 *noise.CipherState
-	out, cs1, cs2, err = c.hs.WriteMessage(out, payload)
+	outlen := len(out)
+	out, cs1, cs2, err = c.hs.WriteMessage(append(out, make([]byte, 4)...), payload)
 	if err != nil {
-		return errs.Wrap(err)
+		return nil, errs.Wrap(err)
 	}
 	c.setCipherStates(cs1, cs2)
 	c.hsResponsibility = false
 	if c.send != nil {
 		c.hs = nil
 	}
-	return c.writeMsg(out)
+	return out, c.frame(out[outlen:], out[outlen+4:])
 }
 
-func (c *Conn) writePayload(b []byte) (err error) {
-	if c.hs != nil && !c.hsResponsibility {
-		err = c.hsRead()
-		if err != nil {
-			return err
-		}
-	}
+func (c *Conn) writeHSPayload(b []byte) (sent bool, err error) {
 	if c.hs != nil {
-		return c.hsWrite(b)
+		c.writeMsgBuf, err = c.hsCreate(c.writeMsgBuf[:0], b)
+		if err != nil {
+			return false, err
+		}
+		_, err = c.Conn.Write(c.writeMsgBuf)
+		return true, errs.Wrap(err)
 	}
-
-	var out []byte // TODO(jt)
-	out, err = c.send.Encrypt(out, nil, b)
-	if err != nil {
-		return errs.Wrap(err)
-	}
-	return c.writeMsg(out)
+	return false, nil
 }
 
 // If a Noise handshake is still occurring (or has yet to occur), the
@@ -207,19 +199,45 @@ func (c *Conn) writePayload(b []byte) (err error) {
 // even if the Noise configuration allows for 0-RTT, the request will only be
 // 0-RTT if the request is 65535 bytes or smaller.
 func (c *Conn) Write(b []byte) (n int, err error) {
-	// TODO(jt): breaking up a large buffer for writes simplifies the noise
-	// code, but we really ought to minimize the number of writes to the
-	// underlying Conn if we want to later implement GSO.
+	for c.hs != nil && len(b) > 0 {
+		if !c.hsResponsibility {
+			err = c.hsRead()
+			if err != nil {
+				return n, err
+			}
+		}
+		if c.hs != nil {
+			l := min(noise.MaxMsgLen, len(b))
+			c.writeMsgBuf, err = c.hsCreate(c.writeMsgBuf[:0], b[:l])
+			if err != nil {
+				return n, err
+			}
+			_, err = c.Conn.Write(c.writeMsgBuf)
+			if err != nil {
+				return n, errs.Wrap(err)
+			}
+			n += l
+			b = b[l:]
+		}
+	}
+
+	c.writeMsgBuf = c.writeMsgBuf[:0]
 	for len(b) > 0 {
+		outlen := len(c.writeMsgBuf)
 		l := min(noise.MaxMsgLen, len(b))
-		err = c.writePayload(b[:l])
+		c.writeMsgBuf, err = c.send.Encrypt(append(c.writeMsgBuf, make([]byte, 4)...), nil, b[:l])
+		if err != nil {
+			return n, errs.Wrap(err)
+		}
+		err = c.frame(c.writeMsgBuf[outlen:], c.writeMsgBuf[outlen+4:])
 		if err != nil {
 			return n, err
 		}
 		n += l
 		b = b[l:]
 	}
-	return n, nil
+	_, err = c.Conn.Write(c.writeMsgBuf)
+	return n, errs.Wrap(err)
 }
 
 // HandshakeComplete returns whether a handshake is complete.
